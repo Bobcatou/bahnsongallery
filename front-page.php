50da3837672a4bb030132424b11228f830e37b05
Bryan Cady <sw@listentothewindmedia.com>
2015-09-12 13:27:34 -0400

    Created second widget area on row 66 and flipped titles with row 1

--- front-page.php
+++ front-page.php
@@ -49,13 +49,12 @@ add_action('genesis_before_content_sidebar_wrap', 'lwm_home_gallery_image');
 //	echo '</div>';  
 //}
 
-//* Hooks Featured Products widget. (Above Footer area)_
-add_action( 'genesis_before_content', 'lwm_featured_items', 20 );
+//* Hooks Featured Products widget. (Above Footer area)
+add_action( 'genesis_before_content', 'lwm_featured_items', 15 );
 	function lwm_featured_items() {
 	echo '<div class="lwm_featured_block">';
 	echo '<div class="wrap lwm_featured_samples">';
-	echo '<h2>Recent Arrived Artwork</h2>';
-
+	echo '<h2>Featured Artwork</h2>';
 		genesis_widget_area( 'lwm_featured', array(
 			'before' => '<div class="lwm_special_items">',
 			'after' => '</div>',	) );
@@ -63,6 +62,24 @@ add_action( 'genesis_before_content', 'lwm_featured_items', 20 );
 	echo '</div>';
 
 }
+
+//* Hooks Featured Products widget. (Above Footer area)
+add_action( 'genesis_before_content', 'lwm_featured_items_row2', 15 );
+	function lwm_featured_items_row2() {
+	echo '<div class="lwm_featured_block-row2">';
+	echo '<div class="wrap lwm_featured_samples-row2">';
+	echo '<h2>Recent Arrived Artwork</h2>';
+		genesis_widget_area( 'lwm_featured-row2', array(
+			'before' => '<div class="lwm_special_items-row2">',
+			'after' => '</div>',	) );
+	echo '</div>';
+	echo '</div>';
+
+}
+
+
+
+
 
 
 
