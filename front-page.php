d1f7fbc48db93f1fdc4cd908e85ce26574fae9f8
Bryan Cady <sw@listentothewindmedia.com>
2015-09-22 13:13:15 -0400

    Changed genesis_widget_area widget name

--- front-page.php
+++ front-page.php
@@ -115,17 +115,22 @@ add_action( 'genesis_before_content_sidebar_wrap', 'lwm_third_row_content', 15 )
 	function lwm_third_row_content() {
 	echo '<div class="lwm_third_row_block">';
 	echo '<div class="wrap third_row_widgets">';
-		genesis_widget_area( 'lwm_left_two_section_3row', array(
+		genesis_widget_area( 'lwm_featured-row3_left', array(
 			'before' => '<div class="lwm_3row_left">',
 			'after' => '</div>',
 	) );
-			genesis_widget_area( 'lwm_right_two_section_3row', array(
+			genesis_widget_area( 'lwm_featured-row3_right', array(
 			'before' => '<div class="lwm_3row_right">',
 			'after' => '</div>',
 	) );
 	echo '</div>';
 	echo '</div>';  
 }
+
+
+
+
+
 
 //* Run the Genesis loop
 genesis();
