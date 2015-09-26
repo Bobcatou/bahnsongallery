c09ca88fc888254a2a1f888839a414109b36a8a1
Bryan Cady <sw@listentothewindmedia.com>
2015-09-26 00:08:48 -0400

    Added enquere #15 to fix alignment issue

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
 
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
-
+	wp_enqueue_script(  'script-name',  get_stylesheet_directory_uri() . '/js/woo_height_align.js',  array(  'jquery'  ), '1.0.0'  );
 }
 
 // Add HTML5 markup structure
