37fb4d5eecaf35e3d0678293481d362295581485
Bryan Cady <sw@listentothewindmedia.com>
2015-11-05 13:24:52 -0500

    Changed brackets on one widgets

--- functions.php
+++ functions.php
@@ -192,17 +192,12 @@ genesis_register_sidebar( array(
 //* 1st Column
 genesis_register_sidebar( array(
 	'id'            => 'lwm_featured-row3_left',
-	'name'          => __( 'Row 3 - Left Side', 'sample' ),
-	'description'   => __( 'Initial Build Coded 1/3 width', 'bahnsengallery' ),
+	'name'          => 'Row 3 - Left Side', 'sample',
+	'description'   => 'Initial Build Coded 1/3 width', 'bahnsengallery',
 ) );
 //* 2nd Column
 genesis_register_sidebar( array(
 	'id'            => 'lwm_featured-row3_right',
-	'name'          => __( 'Row 3 - Right Side', 'sample' ),
-	'description'   => __( 'Initial Build Coded 2/3 width', 'bahnsengallery' ),
+	'name'          => 'Row 3 - Right Side', 'sample',
+	'description'   => 'Initial Build Coded 2/3 width', 'bahnsengallery',
 ) );
-
-
-
-
-
